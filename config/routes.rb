b79b67c476ac2e9dde192d5e504327e0cae41684
Ferdinand Cuisin <fcuisin@gmail.com>
2019-11-18 17:36:58 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: %i[show new create edit update destroy] do
+    resources :purchases, only: %i[new create]
+  end
+  resources :purchases, only: %i[show destroy]
 end
