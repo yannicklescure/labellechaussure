090281836c1592d231f27c398982265188c83877
yannick lescure <yannick.lescure@gmail.com>
2019-11-20 08:23:35 -0500

    add seeds with users and colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+puts 'Cleaning database...'
+Color.destroy_all
+User.destroy_all
+Product.destroy_all
+Purchase.destroy_all
+
+puts 'Creating colors...'
+colors_attributes = [
+  {
+    name: "black"
+  },
+  {
+    name: "white"
+  },
+  {
+    name: "red"
+  },
+  {
+    name: "blue"
+  },
+  {
+    name: "green"
+  },
+  {
+    name: "yellow"
+  },
+  {
+    name: "pink"
+  }
+]
+Color.create!(colors_attributes)
+
+puts 'Creating users...'
+users_attributes = [
+  {
+    email: "ferdinand@email.com",
+    password: "123456"
+  },
+  {
+    email: "pierre@email.com",
+    password: "123456"
+  },
+  {
+    email: "sophie@email.com",
+    password: "123456"
+  },
+  {
+    email: "yannick@email.com",
+    password: "123456"
+  },
+]
+
+User.create!(users_attributes)
+
+# url = 'http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png'
+# cocktail = Cocktail.new(name: 'NES')
+# cocktail.remote_photo_url = url
+# cocktail.save
+puts 'Finished!'
