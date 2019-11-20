32d30eb438247b13cf5305f9f766c799ce171a42
yannick lescure <yannick.lescure@gmail.com>
2019-11-19 19:03:53 -0500

    server without pundit opertional

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,12 +10,36 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase = Purchase.new(purchase_params)
-    @purchase.product = Product.find(params[:product_id])
+    # @product = Product.find(params[:product_id])
+    # @purchase.product = @product
+    @purchase.user = current_user
+    @purchase.done = true
+    if @purchase.save
+      # raise
+      redirect_to purchases_path
+    else
+      # raise
+      render :new
+    end
   end
 
   def new
+    @purchase = Purchase.new
+    @product = Product.find(params[:product_id])
   end
 
   def destroy
+    @purchase = Purchase.find(params[:id])
+    @purchase.destroy
+    redirect_to purchases_path
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(
+      :done, :rating, :first_name, :last_name, :address_1, :address_2,
+      :country, :state, :zip, :product_id
+    )
   end
 end
--- db/migrate/20191119195025_create_purchases_table.rb
+++ db/migrate/20191119195025_create_purchases_table.rb
@@ -2,7 +2,7 @@ class CreatePurchasesTable < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
       t.integer :rating
-      t.boolean :done
+      t.boolean :done, default: false
       t.references :product, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
