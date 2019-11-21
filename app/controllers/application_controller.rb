2d66fcc5f5612d2710647c4bfb2780bd99982dc7
yannick lescure <yannick.lescure@gmail.com>
2019-11-21 08:26:24 -0500

    add truncate string method to unify cards format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   include Pundit
+  include ApplicationHelper
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
