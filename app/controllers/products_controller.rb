726b843c9cf84fa7bc41bfed264a3f860c2b3b83
yannick lescure <yannick.lescure@gmail.com>
2019-11-21 19:11:26 -0500

    minor code change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,6 +55,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :model, :size, :brand, :year, :photo, :color_id)
+    params.require(:product).permit(:title, :description, :price, :model, :size, :brand, :year, :photo, :photo_cache, :color_id)
   end
 end
